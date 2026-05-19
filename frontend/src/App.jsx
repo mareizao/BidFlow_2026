9e68c2713301f37882bb73e6fb8de5394c92a1c6
Juan Esteban Acevedo <juanesacevedop1101@gmail.com>
2026-05-19 09:38:54 -0500

    Solución Context

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 // src/App.jsx
 import { Routes, Route, Navigate } from "react-router-dom"
-import { useAuth } from "./contexts/AuthContext"
+import { useAuth } from "./context/AuthContext"
 import Login from "./pages/Login"
 import Dashboard from "./pages/Dashboard"
 import LicitacionDetalle from "./pages/LicitacionDetalle"
